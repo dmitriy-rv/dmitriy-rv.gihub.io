b15473cd7348df0e5dbddd763c5fd152435afbe9
dmitriy-rv <dimarikrv@gmail.com>
2017-11-15 03:19:30 +0300

    Add files via upload
    new popup

--- js/script.js
+++ js/script.js
@@ -13,6 +13,42 @@ $(".popup-wrap").click(function(){
 	$('.big-wrap').removeClass('blur');
 });
 
+$(".close-button").click(function(){
+	$('.popup-wrap').removeClass('flex');
+	$('.big-wrap').removeClass('blur');
+});
+
 $(".popup-wrap .content").click(function(e) {
 	e.stopImmediatePropagation();
 });
+
+$(".phone-click").click(function(){
+	$('.phone-click').addClass('active');
+	$('.email-click').removeClass('active');
+	$('.vk-click').removeClass('active');
+
+	$('.phone-form').show();
+	$('.email-form').hide();
+	$('.vk-form').hide();
+});
+
+$(".email-click").click(function(){
+	$('.phone-click').removeClass('active');
+	$('.email-click').addClass('active');
+	$('.vk-click').removeClass('active');
+
+	$('.phone-form').hide();
+	$('.email-form').show();
+	$('.vk-form').hide();
+});
+
+$(".vk-click").click(function(){
+	$('.phone-click').removeClass('active');
+	$('.email-click').removeClass('active');
+	$('.vk-click').addClass('active');
+
+	$('.phone-form').hide();
+	$('.email-form').hide();
+	$('.vk-form').show();
+});
+
