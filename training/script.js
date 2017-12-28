6ee9cd812d8cc170d9667c78a5e4ac0322662a75
dryabov <dryabov@roox.com>
2017-12-28 17:58:03 +0300

    new animation

--- training/script.js
+++ training/script.js
@@ -11,3 +11,16 @@ $('.second').click(function(){
 	console.log('click');
 	$('.move').addClass('show');
 });
+
+$('.for_me').click(function(){
+	$('.wrap').addClass('wrap_for_me');
+	$('.content').addClass('content_for_me');
+});
+
+$(window).scroll(function() {
+	console.log($('.for_me').height());
+	if ($('.for_me').offset().top - $(window).scrollTop() -$('.for_me').height()*0.6 < 0){
+		$('.wrap').addClass('wrap_for_me');
+		$('.content').addClass('content_for_me');
+	}
+});
